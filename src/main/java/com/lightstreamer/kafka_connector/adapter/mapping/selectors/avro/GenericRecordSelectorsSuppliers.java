0b50a9c1c5c26aebbe5096f11fd9859e49445c7f
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2024-02-01 11:00:06 +0100

    Checkpoint before spotless.

--- src/main/java/com/lightstreamer/kafka_connector/adapter/mapping/selectors/avro/GenericRecordSelectorsSuppliers.java
+++ src/main/java/com/lightstreamer/kafka_connector/adapter/mapping/selectors/avro/GenericRecordSelectorsSuppliers.java
@@ -1,16 +1,29 @@
+/*
+*
+* Copyright (c) Lightstreamer Srl
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 package com.lightstreamer.kafka_connector.adapter.mapping.selectors.avro;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 
-import org.apache.avro.generic.GenericContainer;
 import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericEnumSymbol;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.util.Utf8;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.slf4j.Logger;
