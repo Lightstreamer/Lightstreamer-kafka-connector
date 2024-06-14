c28c475cdbc7a9ee461eced35faf4c5993e0ed8d
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2024-06-14 12:24:36 +0200

    Add newline at the end of examples/airport-demo/client/web/src/js/const.js

--- examples/airport-demo/client/web/src/js/const.js
+++ examples/airport-demo/client/web/src/js/const.js
@@ -1,5 +1,4 @@
-
-const LS_HOST = "ec2-3-249-233-151.eu-west-1.compute.amazonaws.com";
+const LS_HOST = "localhost";
 const LS_HTTP_PORT = "8080";
 const LS_HTTPS_PORT = "443";
 const LS_ADAPTER_SET = "KafkaConnector";
