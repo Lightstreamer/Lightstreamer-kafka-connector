21cce03912437a42856f0e597d1839ab652ce929
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2024-06-20 11:51:27 +0200

    Add script examples/compose-templates/start-connect.sh

---
+++ examples/compose-templates/start-connect.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source ../utils/helpers.sh
+
+# Build the Lightstreamer Kafka Connector Docker image
+../docker-kafka-connect/build.sh
+
+if [ $? == 0 ]; then
+     export version
+     docker compose -f $(pwd)/docker-compose.yml up --build -d &&
+     sleep 10 && 
+     echo "Services started. Now you can point your browser to http://localhost:8080/QuickStart to see real-time data."
+ fi
