ac3b32dad7f198dab7081dd8e0897d0c00c35cbb
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2024-07-03 17:28:50 +0200

    Fix examples/docker-kafka-connect/build.sh

--- examples/docker-kafka-connect/build.sh
+++ examples/docker-kafka-connect/build.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
+set -eu
 source ../utils/helpers.sh
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 TMP_DIR=${SCRIPT_DIR}/tmp
 
 # Generate the deployment package
 echo "Making the deployment package"
-$_gradle distribuiteConnect
+$_gradle clean distribuiteConnect
 
 mkdir -p ${TMP_DIR}
 cp ${projectDir}/kafka-connector/build/distributions/lightstreamer-kafka-connect-lightstreamer-${version}.zip ${TMP_DIR}
 
 echo "Build the Connect Docker image"
-docker build -t lightstreamer-kafka-connect-lighstreamer-${version} $SCRIPT_DIR --build-arg VERSION=${version}
+docker build -t lightstreamer-kafka-connect-lighstreamer-${version} $SCRIPT_DIR --build-arg VERSION=${version} "$@"
 
-if [ $? == 0 ]; then
-    echo "Launch the image with:"
-    echo "docker run --name lightstreamer-kafka-connect-lighstreamer -d -p 8080:8080 lightstreamer-kafka-connect-lighstreamer-${version}"
-else
-    exit 1
-fi
+echo "Launch the image with:"
+echo "docker run --name lightstreamer-kafka-connect-lighstreamer -d lightstreamer-kafka-connect-lighstreamer-${version}"
