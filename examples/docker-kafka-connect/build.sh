427786ce356340657ebf66f27ce7b7fac70b3307
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2024-08-13 17:42:37 +0200

    Checkpoint

--- examples/docker-kafka-connect/build.sh
+++ examples/docker-kafka-connect/build.sh
@@ -12,5 +12,5 @@ mkdir -p ${TMP_DIR}
 rm -fr ${TMP_DIR}/
 unzip ${projectDir}/kafka-connector/build/distributions/lightstreamer-kafka-connect-lightstreamer-${version}.zip -d ${TMP_DIR}
 
-echo "Build the Kafka Connect Lightstreamer Sink Connector Docker image"
+echo "Building the Kafka Connect Lightstreamer Sink Connector Docker image"
 docker build -t kafka-connect-lightstreamer-${version} $SCRIPT_DIR --build-arg VERSION=${version} "$@"
