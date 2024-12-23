c0a06fc65081cdecdca8139be8a4a6f58acb93a9
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2024-12-23 17:43:21 +0100

    Add support for reg subscriptions toConsumerWrapperImpl.java

--- kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/adapters/consumers/wrapper/ConsumerWrapperImpl.java
+++ kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/adapters/consumers/wrapper/ConsumerWrapperImpl.java
@@ -180,50 +180,49 @@ class ConsumerWrapperImpl<K, V> implements ConsumerWrapper<K, V> {
     protected boolean subscribed() {
         ItemTemplates<K, V> templates = config.itemTemplates();
         if (templates.isRegexEnabled()) {
-            String regex = templates.topics().stream().collect(Collectors.joining("|"));
-            log.debug("Subscribing to the requested pattern {}", regex);
-            consumer.subscribe(Pattern.compile(regex), offsetService);
+            Pattern pattern = templates.subscriptionPattern().get();
+            log.debug("Subscribing to the requested pattern {}", pattern.pattern());
+            consumer.subscribe(pattern, offsetService);
             return true;
-        } else {
-            // Original requested topics.
-            Set<String> topics = new HashSet<>(templates.topics());
-            log.atInfo().log("Subscribing to requested topics [{}]", topics);
-            log.atDebug().log("Checking existing topics on Kafka");
+        }
+        // Original requested topics.
+        Set<String> topics = new HashSet<>(templates.topics());
+        log.atInfo().log("Subscribing to requested topics [{}]", topics);
+        log.atDebug().log("Checking existing topics on Kafka");
 
-            // Check the actual available topics on Kafka.
-            try (AdminInterface admin = adminFactory.apply(config.consumerProperties())) {
-                ListTopicsOptions options = new ListTopicsOptions();
-                options.timeoutMs(30000);
+        // Check the actual available topics on Kafka.
+        try (AdminInterface admin = adminFactory.apply(config.consumerProperties())) {
+            ListTopicsOptions options = new ListTopicsOptions();
+            options.timeoutMs(30000);
 
-                // Retain from the original requestes topics the available ones.
-                Set<String> existingTopics = admin.listTopics(options);
-                boolean notAllPresent = topics.retainAll(existingTopics);
+            // Retain from the original requestes topics the available ones.
+            Set<String> existingTopics = admin.listTopics(options);
+            boolean notAllPresent = topics.retainAll(existingTopics);
 
-                // Can't subscribe at all. Force unsubscription and exit the loop.
-                if (topics.isEmpty()) {
-                    log.atWarn().log("Not found requested topics");
-                    metadataListener.forceUnsubscriptionAll();
-                    return false;
-                }
-
-                // Just warn that not all requested topics can be subscribed.
-                if (notAllPresent) {
-                    String loggableTopics =
-                            topics.stream()
-                                    .map(s -> "\"%s\"".formatted(s))
-                                    .collect(Collectors.joining(","));
-                    log.atWarn()
-                            .log(
-                                    "Actually subscribing to the following existing topics [{}]",
-                                    loggableTopics);
-                }
-                consumer.subscribe(topics, offsetService);
-                return true;
-            } catch (Exception e) {
-                log.atError().setCause(e).log();
+            // Can't subscribe at all. Force unsubscription and exit the loop.
+            if (topics.isEmpty()) {
+                log.atWarn().log("Not found requested topics");
                 metadataListener.forceUnsubscriptionAll();
                 return false;
             }
+
+            // Just warn that not all requested topics can be subscribed.
+            if (notAllPresent) {
+                String loggableTopics =
+                        topics.stream()
+                                .map(s -> "\"%s\"".formatted(s))
+                                .collect(Collectors.joining(","));
+                log.atWarn()
+                        .log(
+                                "Actually subscribing to the following existing topics [{}]",
+                                loggableTopics);
+            }
+            consumer.subscribe(topics, offsetService);
+            return true;
+        } catch (Exception e) {
+            log.atError().setCause(e).log();
+            metadataListener.forceUnsubscriptionAll();
+            return false;
         }
     }
 
