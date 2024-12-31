4ce1552d92c4ee5bc9ff7ba46c7fd716a1ffdfcd
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2024-12-31 18:27:12 +0100

    Remove unused Supplier import from DataExtractorSupport.java

--- kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/common/mapping/selectors/DataExtractorSupport.java
+++ kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/common/mapping/selectors/DataExtractorSupport.java
@@ -28,7 +28,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
