a0f52a46cf54bc675c0a7f9b3533dafdf929d103
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-04-01 18:34:20 +0200

    Remove unwanted resources from the examples folder

--- examples/kvp-demo/producer/start-publisher.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -ex
-java -jar build/libs/quickstart-producer-all.jar --bootstrap-servers pkc-z9doz.eu-west-1.aws.confluent.cloud:9092 --topic stocks --config-file producer.properties
