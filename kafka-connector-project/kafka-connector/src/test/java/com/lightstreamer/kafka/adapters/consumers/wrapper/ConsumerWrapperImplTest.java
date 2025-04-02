cb30007243a193525d1b24369098ef6a118ad242
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-04-02 11:26:06 +0200

    Make subscribedItems field final in ConsumerWrapperImplTest for immutability

--- kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/consumers/wrapper/ConsumerWrapperImplTest.java
+++ kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/consumers/wrapper/ConsumerWrapperImplTest.java
@@ -65,7 +65,7 @@ public class ConsumerWrapperImplTest {
 
     private MockMetadataListener metadataListener;
     private MockItemEventListener itemEventListener;
-    private Collection<SubscribedItem> subscribedItems = Collections.emptyList();
+    private final Collection<SubscribedItem> subscribedItems = Collections.emptyList();
     private MockConsumer<String, String> mockConsumer;
     private final OffsetResetStrategy resetStrategy = OffsetResetStrategy.EARLIEST;
 
