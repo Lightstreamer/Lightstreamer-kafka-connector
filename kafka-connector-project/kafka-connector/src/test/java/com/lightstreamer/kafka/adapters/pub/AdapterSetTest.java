69df17ace87f204b0e185c0dbebbe9b261d50a2a
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-04-07 18:41:05 +0200

    Add field.key and field.command to KafkaConnectorDataAdapter initialization

--- kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/pub/AdapterSetTest.java
+++ kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/pub/AdapterSetTest.java
@@ -153,7 +153,13 @@ public class AdapterSetTest {
         KafkaConnectorDataAdapter connectorDataAdapter2 = new KafkaConnectorDataAdapter();
         connectorDataAdapter2.init(
                 ConnectorConfigProvider.minimalConfigWith(
-                        Map.of(ConnectorConfig.FIELDS_EVALUATE_AS_COMMAND_ENABLE, "true")),
+                        Map.of(
+                                ConnectorConfig.FIELDS_EVALUATE_AS_COMMAND_ENABLE,
+                                "true",
+                                "field.key",
+                                "#{KEY}",
+                                "field.command",
+                                "#{VALUE}")),
                 adapterDir.toFile());
         connectorDataAdapter2.setListener(new Mocks.MockItemEventListener());
         assertThat(connectorDataAdapter2.isSnapshotAvailable("anItem")).isTrue();
