2f91b93534ddebdc4e487cdf26f49bdf99d40ead
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-04-11 19:33:00 +0200

    Add unit tests for DynamicMessage deserialization in DynamicMessageDeserializerTest

---
+++ kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/mapping/selectors/protobuf/DynamicMessageDeserializerTest.java
@@ -0,0 +1,104 @@
+
+/*
+ * Copyright (C) 2024 Lightstreamer Srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.lightstreamer.kafka.adapters.mapping.selectors.protobuf;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.lightstreamer.kafka.adapters.config.ConnectorConfig.RECORD_KEY_EVALUATOR_SCHEMA_REGISTRY_ENABLE;
+import static com.lightstreamer.kafka.adapters.config.ConnectorConfig.RECORD_KEY_EVALUATOR_TYPE;
+import static com.lightstreamer.kafka.adapters.config.ConnectorConfig.RECORD_VALUE_EVALUATOR_SCHEMA_REGISTRY_ENABLE;
+import static com.lightstreamer.kafka.adapters.config.ConnectorConfig.RECORD_VALUE_EVALUATOR_TYPE;
+import static com.lightstreamer.kafka.adapters.config.specs.ConfigTypes.EvaluatorType.PROTOBUF;
+
+import com.google.protobuf.DynamicMessage;
+import com.lightstreamer.kafka.adapters.config.ConnectorConfig;
+import com.lightstreamer.kafka.adapters.config.SchemaRegistryConfigs;
+import com.lightstreamer.kafka.test_utils.ConnectorConfigProvider;
+
+import io.confluent.kafka.serializers.json.KafkaJsonSchemaDeserializer;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer;
+
+import org.apache.kafka.common.serialization.Deserializer;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class DynamicMessageDeserializerTest {
+
+    @Test
+    public void shouldGeKeyDeserializer() {
+        ConnectorConfig config =
+                ConnectorConfigProvider.minimalWith(
+                        Map.of(
+                                RECORD_KEY_EVALUATOR_TYPE,
+                                PROTOBUF.toString(),
+                                RECORD_KEY_EVALUATOR_SCHEMA_REGISTRY_ENABLE,
+                                "true",
+                                SchemaRegistryConfigs.URL,
+                                "http://localhost:8080"));
+
+        try (Deserializer<DynamicMessage> deserializer =
+                DynamicMessageDeserializers.KeyDeserializer(config)) {
+            assertThat(deserializer.getClass()).isEqualTo(KafkaProtobufDeserializer.class);
+        }
+    }
+
+    @Test
+    public void shouldGetValueDeserializer() {
+        ConnectorConfig config =
+                ConnectorConfigProvider.minimalWith(
+                        Map.of(
+                                RECORD_VALUE_EVALUATOR_TYPE,
+                                PROTOBUF.toString(),
+                                RECORD_VALUE_EVALUATOR_SCHEMA_REGISTRY_ENABLE,
+                                "true",
+                                SchemaRegistryConfigs.URL,
+                                "http://localhost:8080"));
+
+        try (Deserializer<DynamicMessage> deserializer =
+                DynamicMessageDeserializers.ValueDeserializer(config)) {
+            assertThat(deserializer.getClass()).isEqualTo(KafkaJsonSchemaDeserializer.class);
+        }
+    }
+
+    @Test
+    public void shouldGetKeyAndValueDeserialize() {
+        ConnectorConfig config =
+                ConnectorConfigProvider.minimalWith(
+                        Map.of(
+                                RECORD_KEY_EVALUATOR_TYPE,
+                                PROTOBUF.toString(),
+                                RECORD_KEY_EVALUATOR_SCHEMA_REGISTRY_ENABLE,
+                                "true",
+                                RECORD_VALUE_EVALUATOR_TYPE,
+                                PROTOBUF.toString(),
+                                RECORD_VALUE_EVALUATOR_SCHEMA_REGISTRY_ENABLE,
+                                "true",
+                                SchemaRegistryConfigs.URL,
+                                "http://localhost:8080"));
+
+        try (Deserializer<DynamicMessage> deserializer =
+                DynamicMessageDeserializers.KeyDeserializer(config)) {
+            assertThat(deserializer.getClass()).isEqualTo(KafkaProtobufDeserializer.class);
+        }
+
+        try (Deserializer<DynamicMessage> deserializer =
+                DynamicMessageDeserializers.ValueDeserializer(config)) {
+            assertThat(deserializer.getClass()).isEqualTo(KafkaProtobufDeserializer.class);
+        }
+    }
+}
