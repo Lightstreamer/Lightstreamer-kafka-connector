2a3979d660b406b724c57a91dc493b17a5f47e1e
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-06-24 12:21:08 +0200

    Refactor SchemaAndValues to introduce BuildableSchemaAndValues for enhanced value management and improve NOP schema handling

--- kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/common/mapping/selectors/SchemaAndValues.java
+++ kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/common/mapping/selectors/SchemaAndValues.java
@@ -17,9 +17,11 @@
 
 package com.lightstreamer.kafka.common.mapping.selectors;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public interface SchemaAndValues {
 
@@ -27,16 +29,30 @@ public interface SchemaAndValues {
 
     Map<String, String> values();
 
+    default String asText() {
+        if (schema().keys().isEmpty()) {
+            return schema().name();
+        }
+
+        String suffix =
+                new TreeMap<>(values())
+                        .entrySet().stream()
+                                .map(entry -> entry.getKey() + "=" + entry.getValue())
+                                .collect(Collectors.joining(","));
+
+        return String.format("%s-[%s]", schema().name(), suffix);
+    }
+
     default boolean matches(SchemaAndValues other) {
         return schema().matches(other.schema()) && values().equals(other.values());
     }
 
-    static SchemaAndValues from(Schema schema, Map<String, String> values) {
-        return new DefaultSchemaAndValues(schema, values);
-    }
-
     static SchemaAndValues from(String schemaName, Map<String, String> values) {
         Schema schema = Schema.from(schemaName, values.keySet());
+        return from(schema, values);
+    }
+
+    static SchemaAndValues from(Schema schema, Map<String, String> values) {
         return new DefaultSchemaAndValues(schema, values);
     }
 
@@ -45,16 +61,26 @@ public interface SchemaAndValues {
     }
 }
 
-class DefaultSchemaAndValues implements SchemaAndValues {
-
-    static SchemaAndValues NOP = new DefaultSchemaAndValues(Schema.nop(), Collections.emptyMap());
+class BuildableSchemaAndValues implements SchemaAndValues {
 
     private final Schema schema;
     private final Map<String, String> values;
 
-    DefaultSchemaAndValues(Schema schema, Map<String, String> values) {
+    public BuildableSchemaAndValues(Schema schema) {
         this.schema = schema;
-        this.values = Collections.unmodifiableMap(values);
+        this.values = new HashMap<>();
+    }
+
+    public BuildableSchemaAndValues addValue(String key, String value) {
+        if (schema.keys().contains(key)) {
+            values.put(key, value);
+        }
+        return this;
+    }
+
+    public BuildableSchemaAndValues addValueNoKeyCheck(String key, String value) {
+        values.put(key, value);
+        return this;
     }
 
     @Override
@@ -76,9 +102,47 @@ class DefaultSchemaAndValues implements SchemaAndValues {
     public int hashCode() {
         return Objects.hash(schema, values);
     }
+}
+
+class DefaultSchemaAndValues implements SchemaAndValues {
+
+    /** A constant representing a no-operation (NOP) schema with no values. */
+    static SchemaAndValues NOP = new DefaultSchemaAndValues(Schema.nop());
+
+    private final Schema schema;
+    private final Map<String, String> values;
+
+    DefaultSchemaAndValues(Schema schema, Map<String, String> values) {
+        this.schema = schema;
+        this.values = values;
+    }
+
+    /**
+     * This constructor is used only for the NOP schema. The passed empty map is modifiable to allow
+     * decoration needed in the command mode case, where additional keys such as "command" and
+     * "keys" are added
+     */
+    private DefaultSchemaAndValues(Schema schema) {
+        this(schema, new HashMap<>());
+    }
 
     @Override
-    public String toString() {
-        return String.format("(%s-<%s>)", schema.name(), values);
+    public Schema schema() {
+        return schema;
+    }
+
+    @Override
+    public Map<String, String> values() {
+        return values;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        return obj instanceof SchemaAndValues other && this.matches(other);
+    }
+
+    public int hashCode() {
+        return Objects.hash(schema, values);
     }
 }
