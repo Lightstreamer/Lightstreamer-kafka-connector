b9daf4d2c3381926620d8acb61817508ecff908c
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-06-26 11:17:39 +0200

    Refactor SubscriptionsHandler to replace ConsumerTrigger with KafkaConsumerWrapper for improved clarity and consistency

--- kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/adapters/consumers/SubscriptionsHandler.java
+++ kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/adapters/consumers/SubscriptionsHandler.java
@@ -20,13 +20,13 @@ package com.lightstreamer.kafka.adapters.consumers;
 import com.lightstreamer.interfaces.data.ItemEventListener;
 import com.lightstreamer.interfaces.data.SubscriptionException;
 import com.lightstreamer.kafka.adapters.commons.MetadataListener;
-import com.lightstreamer.kafka.adapters.consumers.trigger.ConsumerTrigger;
-import com.lightstreamer.kafka.adapters.consumers.trigger.ConsumerTrigger.ConsumerTriggerConfig;
-import com.lightstreamer.kafka.adapters.consumers.wrapper.ConsumerWrapper;
+import com.lightstreamer.kafka.adapters.consumers.wrapper.KafkaConsumerWrapper;
+import com.lightstreamer.kafka.adapters.consumers.wrapper.KafkaConsumerWrapper.Config;
 import com.lightstreamer.kafka.common.mapping.Items.Item;
-import com.lightstreamer.kafka.common.mapping.Items.SubscribedItems;
 
-import java.util.function.Function;
+import org.apache.kafka.clients.consumer.Consumer;
+
+import java.util.function.Supplier;
 
 public interface SubscriptionsHandler {
 
@@ -34,29 +34,33 @@ public interface SubscriptionsHandler {
      * Creates a new {@code SubscriptionWrapper} instance with the specified configuration, metadata
      * listener, event listener, and startup consumption flag.
      *
-     * @param config the consumer trigger configuration
+     * @param consumerConfig the consumer configuration
      * @param metadataListener the metadata listener to be used
      * @param eventListener the item event listener to be notified
+     * @param consumer a supplier that provides the Kafka consumer instance
      * @param consumeAtStartup whether to start consuming messages from Kafka at startup
      * @return a new {@code SubscriptionWrapper} instance
      */
-    static SubscriptionsHandler create(
-            ConsumerTriggerConfig<?, ?> config,
+    static <K, V> SubscriptionsHandler create(
+            Config<K, V> consumerConfig,
             MetadataListener metadataListener,
             ItemEventListener eventListener,
+            Supplier<Consumer<K, V>> consumer,
             boolean consumeAtStartup) {
         return SubscriptionsHandlerSupport.create(
-                () -> ConsumerTrigger.create(config, metadataListener, eventListener),
-                consumeAtStartup);
+                consumerConfig, metadataListener, eventListener, consumer, consumeAtStartup);
     }
 
     static <K, V> SubscriptionsHandler create(
-            ConsumerTriggerConfig<K, V> config,
+            Config<K, V> consumerConfig,
             MetadataListener metadataListener,
-            Function<SubscribedItems, ConsumerWrapper<K, V>> consumerWrapper,
+            ItemEventListener eventListener,
             boolean consumeAtStartup) {
-        return SubscriptionsHandlerSupport.create(
-                () -> ConsumerTrigger.create(config, metadataListener, consumerWrapper),
+        return create(
+                consumerConfig,
+                metadataListener,
+                eventListener,
+                KafkaConsumerWrapper.defaultConsumerSupplier(consumerConfig),
                 consumeAtStartup);
     }
 
