3e8f11e540f065d78d857f568ca29bc6c306f02d
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-07-15 18:11:02 +0200

    Refactor RecordProcessorTest to enhance command processing tests and improve clarity

--- kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/consumers/processor/RecordProcessorTest.java
+++ kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/consumers/processor/RecordProcessorTest.java
@@ -41,15 +41,19 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
+import java.util.stream.Stream;
 
 public class RecordProcessorTest {
 
@@ -130,6 +134,25 @@ public class RecordProcessorTest {
 
         // The collection of subscribable items
         this.subscribedItems = new HashSet<>();
+    }
+
+    private static RecordMapper<String, String> buildMapperForCommandMode()
+            throws ExtractionException {
+        return builder()
+                .withTemplateExtractor(
+                        TEST_TOPIC,
+                        extractor(String(), "item1", Collections.emptyMap(), false, false))
+                .withTemplateExtractor(
+                        TEST_TOPIC,
+                        extractor(String(), "item2", Collections.emptyMap(), false, false))
+                .withFieldExtractor(
+                        extractor(
+                                String(),
+                                "fields",
+                                Map.of("key", Wrapped("#{KEY}"), "command", Wrapped("#{VALUE}")),
+                                false,
+                                false))
+                .build();
     }
 
     RecordProcessor<String, String> processor(
@@ -237,93 +260,252 @@ public class RecordProcessorTest {
         assertThat(smartConsumer.getLastUpdates()).containsExactly("command", "DELETE");
     }
 
+    static Stream<Arguments> commands() {
+        return Stream.of(
+                Arguments.of("ADD", false, 1),
+                Arguments.of("UPDATE", false, 1),
+                Arguments.of("DELETE", false, 1),
+                Arguments.of("ADD", true, 2));
+                // Arguments.of("UPDATE", true),
+                // Arguments.of("DELETE", true));
+    }
+
     @ParameterizedTest
-    @ValueSource(strings = {"ADD", "UPDATE", "DELETE"})
-    public void shouldProcessRecordWithCommand(String command) throws ExtractionException {
-        this.mapper =
-                builder()
-                        .withTemplateExtractor(
-                                TEST_TOPIC,
-                                extractor(String(), "item1", Collections.emptyMap(), false, false))
-                        .withTemplateExtractor(
-                                TEST_TOPIC,
-                                extractor(String(), "item2", Collections.emptyMap(), false, false))
-                        .withFieldExtractor(
-                                extractor(
-                                        String(),
-                                        "key",
-                                        Map.of(
-                                                "key",
-                                                Wrapped("#{KEY}"),
-                                                "command",
-                                                Wrapped("#{VALUE}")),
-                                        false,
-                                        false))
-                        .build();
+    @MethodSource("commands")
+    public void shouldProcessRecordWithAdmittedCommands(String command, boolean allowImplicitItems, int expectedUpdates)
+            throws ExtractionException {
+        EventConsumer consumer = allowImplicitItems ? legacyConsumer : smartConsumer;
+        this.mapper = buildMapperForCommandMode();
         processor =
                 processor(
-                        SubscribedItems.of(subscribedItems),
+                        SubscribedItems.of(subscribedItems, allowImplicitItems),
                         ProcessUpdatesStrategy.commandStrategy());
         assertThat(processor.processUpdatesType()).isEqualTo(ProcessUpdatesType.COMMAND);
 
-        // Subscribe to "item1" and process the record
-        subscribedItems.add(Items.subscribedFrom("item1", new Object()));
+        if (!allowImplicitItems) {
+            // Subscribe to "item1" and process the record
+            subscribedItems.add(Items.subscribedFrom("item1", new Object()));
+        }
 
         ConsumerRecord<String, String> record = Records.ConsumerRecord(TEST_TOPIC, "aKey", command);
         processor.process(record);
 
+        // Verify that the nor clear snapshot neither end of snapshot were called
+        if (allowImplicitItems) {
+            assertThat(listener.legacyClearSnapshotCalled()).isFalse();
+            assertThat(listener.legacyEndOfSnapshotCalled()).isFalse();
+
+        } else {
+            assertThat(listener.smartClearSnapshotCalled()).isFalse();
+            assertThat(listener.smartEndOfSnapshotCalled()).isFalse();
+        }
+
         // Verify that the real-time update has been routed
-        assertThat(smartConsumer.getCounter()).isEqualTo(1);
+        assertThat(consumer.getCounter()).isEqualTo(expectedUpdates);
+        assertThat(consumer.getLastUpdates()).containsExactly("key", "aKey", "command", command);
+
         // Verify that the update has been routed as a snapshot
-        assertThat(smartConsumer.isSnapshotEvent()).isTrue();
-        assertThat(smartConsumer.getLastUpdates())
-                .containsExactly("key", "aKey", "command", command);
+        assertThat(consumer.isSnapshotEvent()).isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"CS", "EOS"})
-    public void shouldProcessRecordWithSnapshotCommand(String command) throws ExtractionException {
-        this.mapper =
-                builder()
-                        .withTemplateExtractor(
-                                TEST_TOPIC,
-                                extractor(String(), "item1", Collections.emptyMap(), false, false))
-                        .withTemplateExtractor(
-                                TEST_TOPIC,
-                                extractor(String(), "item2", Collections.emptyMap(), false, false))
-                        .withFieldExtractor(
-                                extractor(
-                                        String(),
-                                        "key",
-                                        Map.of(
-                                                "key",
-                                                Wrapped("#{KEY}"),
-                                                "command",
-                                                Wrapped("#{VALUE}")),
-                                        false,
-                                        false))
-                        .build();
+    public void shouldNotProcessRecordWithNotAdmittedCommand(String command)
+            throws ExtractionException {
+        this.mapper = buildMapperForCommandMode();
         processor =
                 processor(
                         SubscribedItems.of(subscribedItems),
                         ProcessUpdatesStrategy.commandStrategy());
-        assertThat(processor.processUpdatesType()).isEqualTo(ProcessUpdatesType.COMMAND);
 
         // Subscribe to "item1" and process the record
         subscribedItems.add(Items.subscribedFrom("item1", new Object()));
-
-        ConsumerRecord<String, String> record =
-                Records.ConsumerRecord(TEST_TOPIC, "snapshot", command);
+        ConsumerRecord<String, String> record = Records.ConsumerRecord(TEST_TOPIC, "aKey", command);
         processor.process(record);
 
-        // Verify that the snapshot command has been routed
-        if (command.equals("CS")) {
-            assertThat(listener.smartClearSnapshotCalled()).isTrue();
-        } else {
-            assertThat(listener.smartEndOfSnapshotCalled()).isTrue();
-        }
-
-        // Verify that the no real-time update occurred
+        // Verify that the real-time update has NOT been routed
         assertThat(smartConsumer.getCounter()).isEqualTo(0);
+
+        // Double check that the update has NOT been routed as a snapshot
+        assertThat(smartConsumer.isSnapshotEvent()).isFalse();
+
+        // Verify that the nor clearSnapshot neither endOfSnapshot were called
+        assertThat(listener.smartClearSnapshotCalled()).isFalse();
+        assertThat(listener.smartEndOfSnapshotCalled()).isFalse();
+    }
+
+    @Test
+    public void shouldProcessRecordWithClearSnapshotCommand() throws ExtractionException {
+        this.mapper = buildMapperForCommandMode();
+        processor =
+                processor(
+                        SubscribedItems.of(subscribedItems),
+                        ProcessUpdatesStrategy.commandStrategy());
+
+        // Subscribe to "item1" and process the record
+        SubscribedItem item = Items.subscribedFrom("item1", new Object());
+        assertThat(item.isSnapshot()).isTrue();
+        subscribedItems.add(item);
+
+        ConsumerRecord<String, String> record =
+                Records.ConsumerRecord(TEST_TOPIC, "snapshot", "CS");
+        processor.process(record);
+
+        // Verify that only clearSnapshot was called
+        assertThat(listener.smartClearSnapshotCalled()).isTrue();
+        assertThat(listener.smartEndOfSnapshotCalled()).isFalse();
+
+        // Verify that the real-time update has NOT been routed
+        assertThat(smartConsumer.getCounter()).isEqualTo(0);
+
+        // Verify that the item keeps being a snapshot
+        assertThat(item.isSnapshot()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ADD", "UPDATE", "DELETE"})
+    public void shouldNotProcessRecordWithWrongCommandForSnapshot(String wrongCommand)
+            throws ExtractionException {
+        this.mapper = buildMapperForCommandMode();
+        processor =
+                processor(
+                        SubscribedItems.of(subscribedItems),
+                        ProcessUpdatesStrategy.commandStrategy());
+
+        // Subscribe to "item1" and process the record
+        SubscribedItem item = Items.subscribedFrom("item1", new Object());
+        assertThat(item.isSnapshot()).isTrue();
+        subscribedItems.add(item);
+
+        // Consume a record with a "snapshot" key and a wrong command
+        ConsumerRecord<String, String> record =
+                Records.ConsumerRecord(TEST_TOPIC, "snapshot", wrongCommand);
+        processor.process(record);
+
+        // Verify that the nor clear snapshot neither end of snapshot were called
+        assertThat(listener.smartClearSnapshotCalled()).isFalse();
+        assertThat(listener.smartEndOfSnapshotCalled()).isFalse();
+
+        // Verify that the real-time update has NOT been routed
+        assertThat(smartConsumer.getCounter()).isEqualTo(0);
+
+        // Verify that the item is still a snapshot
+        assertThat(item.isSnapshot()).isTrue();
+    }
+
+    @Test
+    public void shouldProcessRecordWithEndOfSnapshotCommand() throws ExtractionException {
+        this.mapper = buildMapperForCommandMode();
+        processor =
+                processor(
+                        SubscribedItems.of(subscribedItems),
+                        ProcessUpdatesStrategy.commandStrategy());
+
+        // Subscribe to "item1" and process the record
+        SubscribedItem item = Items.subscribedFrom("item1", new Object());
+        assertThat(item.isSnapshot()).isTrue();
+        subscribedItems.add(item);
+
+        ConsumerRecord<String, String> record =
+                Records.ConsumerRecord(TEST_TOPIC, "snapshot", "EOS");
+        processor.process(record);
+
+        // Verify only end of snapshot was called
+        assertThat(listener.smartClearSnapshotCalled()).isFalse();
+        assertThat(listener.smartEndOfSnapshotCalled()).isTrue();
+
+        // Verify that the real-time update has NOT been routed
+        assertThat(smartConsumer.getCounter()).isEqualTo(0);
+
+        // Verify that the item is no longer a snapshot
+        assertThat(item.isSnapshot()).isFalse();
+    }
+
+    @Test
+    public void shouldNotTriggerSnapshotEventAfterEOS() throws ExtractionException {
+        this.mapper = buildMapperForCommandMode();
+        processor =
+                processor(
+                        SubscribedItems.of(subscribedItems),
+                        ProcessUpdatesStrategy.commandStrategy());
+
+        // Subscribe to "item1"
+        SubscribedItem item = Items.subscribedFrom("item1", new Object());
+        subscribedItems.add(item);
+
+        // Process a record containing a regular command
+        var addRecord = Records.ConsumerRecord(TEST_TOPIC, "aKey", "ADD");
+        processor.process(addRecord);
+
+        // Verify that the real-time update has been routed
+        assertThat(smartConsumer.getCounter()).isEqualTo(1);
+
+        // Verify that the update has been routed as a snapshot
+        assertThat(smartConsumer.isSnapshotEvent()).isTrue();
+
+        // Then process a record containing an end of snapshot command
+        var eosRecord = Records.ConsumerRecord(TEST_TOPIC, "snapshot", "EOS");
+        processor.process(eosRecord);
+
+        // Verify that the end of snapshot was called
+        assertThat(listener.smartEndOfSnapshotCalled()).isTrue();
+
+        // Finally, process a records containing regulars commands, which should not trigger
+        // snapshot events
+        int currentCounter = smartConsumer.getCounter();
+        for (String command : List.of("ADD", "UPDATE", "DELETE")) {
+            currentCounter++;
+            var record = Records.ConsumerRecord(TEST_TOPIC, "aKey", command);
+            processor.process(record);
+            // Verify that the real-time update has been routed
+            assertThat(smartConsumer.getCounter()).isEqualTo(currentCounter);
+            // Verify that the update has NOT been routed as a snapshot
+            assertThat(smartConsumer.isSnapshotEvent()).isFalse();
+
+            // Verify that the item is no longer a snapshot
+            assertThat(item.isSnapshot()).isFalse();
+        }
+    }
+
+    @Test
+    public void shouldKeepSendingSnapshotAfterCS() throws ExtractionException {
+        this.mapper = buildMapperForCommandMode();
+        processor =
+                processor(
+                        SubscribedItems.of(subscribedItems),
+                        ProcessUpdatesStrategy.commandStrategy());
+
+        // Subscribe to "item1"
+        SubscribedItem item = Items.subscribedFrom("item1", new Object());
+        subscribedItems.add(item);
+
+        // Process a record containing a regular command
+        var addRecord = Records.ConsumerRecord(TEST_TOPIC, "aKey", "ADD");
+        processor.process(addRecord);
+
+        assertThat(smartConsumer.getCounter()).isEqualTo(1);
+        assertThat(smartConsumer.isSnapshotEvent()).isTrue();
+
+        // Then process a record containing a clear snapshot command
+        var clsRecord = Records.ConsumerRecord(TEST_TOPIC, "snapshot", "CS");
+        processor.process(clsRecord);
+        assertThat(listener.smartClearSnapshotCalled()).isTrue();
+
+        // Finally, process a records containing regulars commands, which should still trigger
+        // snapshot events
+        int currentCounter = smartConsumer.getCounter();
+        for (String command : List.of("ADD", "UPDATE", "DELETE")) {
+            currentCounter++;
+            var record = Records.ConsumerRecord(TEST_TOPIC, "aKey", command);
+            processor.process(record);
+            // Verify that the real-time update has been routed
+            assertThat(smartConsumer.getCounter()).isEqualTo(currentCounter);
+
+            // Verify that the update has been routed as a snapshot
+            assertThat(smartConsumer.isSnapshotEvent()).isTrue();
+
+            // Verify that the item is still a snapshot
+            assertThat(item.isSnapshot()).isTrue();
+        }
     }
 }
