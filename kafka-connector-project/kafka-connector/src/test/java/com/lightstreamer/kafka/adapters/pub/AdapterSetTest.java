c50dcbbee2bbfc74646dff327edd6195627b5166
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-10-10 17:18:54 +0200

    Add parameterized tests for item remapping and retrieval in AdapterSetTest

--- kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/pub/AdapterSetTest.java
+++ kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/pub/AdapterSetTest.java
@@ -23,6 +23,7 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import com.lightstreamer.interfaces.metadata.CreditsException;
+import com.lightstreamer.interfaces.metadata.ItemsException;
 import com.lightstreamer.interfaces.metadata.MetadataProviderException;
 import com.lightstreamer.interfaces.metadata.Mode;
 import com.lightstreamer.interfaces.metadata.NotificationException;
@@ -37,14 +38,20 @@ import com.lightstreamer.kafka.test_utils.Mocks;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class AdapterSetTest {
 
@@ -226,6 +233,44 @@ public class AdapterSetTest {
         assertThat(closedTables).isNotNull();
         assertThat(closedTables.sessionId()).isEqualTo("sessionId");
         assertThat(closedTables.tables()).isEqualTo(tables);
+    }
+
+    static Stream<Arguments> provideRemapItems() {
+        return Stream.of(
+                Arguments.of("item1 item2", List.of("item1", "item2")),
+                Arguments.of(" item1   item2 ", List.of("item1", "item2")),
+                Arguments.of("item1", List.of("item1")),
+                Arguments.of("item-1 item_2 item.3", List.of("item-1", "item_2", "item.3")),
+                Arguments.of("", Collections.emptyList()),
+                Arguments.of("   ", Collections.emptyList()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideRemapItems")
+    void shouldRemapItems(String input, List<String> expected)
+            throws MetadataProviderException, ItemsException {
+        doInit();
+
+        String[] items =
+                connectorMetadataAdapter.remapItems("user", "sessionId", input, "dataAdapter");
+        assertThat(items).asList().isEqualTo(expected);
+    }
+
+    static Stream<Arguments> provideGetIItems() {
+        return Stream.of(
+                Arguments.of("item-[c=1,a=2,b=3] item-[b=1,c=2,a=3]", List.of("item-[a=2,b=3,c=1]", "item-[a=3,b=1,c=2]")),
+                Arguments.of("", Collections.emptyList()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideGetIItems")
+    void shouldGetItems(String input, List<String> expected)
+            throws MetadataProviderException, ItemsException {
+        doInit();
+
+        String[] items =
+                connectorMetadataAdapter.getItems("user", "sessionId", input, "dataAdapter");
+        assertThat(items).asList().isEqualTo(expected);
     }
 
     private TableInfo[] mkTable(String adapterName, Mode mode) {
