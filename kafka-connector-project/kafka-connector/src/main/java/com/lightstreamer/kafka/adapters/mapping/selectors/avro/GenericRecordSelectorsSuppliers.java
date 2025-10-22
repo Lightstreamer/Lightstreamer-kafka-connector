83097e2b107a402a828495181516592be6fe8829
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.come>
2025-10-22 18:40:37 +0200

    Refactor GenericRecordSelectorsSuppliers: streamline AvroNode creation and enhance type handling

--- kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/adapters/mapping/selectors/avro/GenericRecordSelectorsSuppliers.java
+++ kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/adapters/mapping/selectors/avro/GenericRecordSelectorsSuppliers.java
@@ -41,46 +41,47 @@ import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.util.Utf8;
 import org.apache.kafka.common.serialization.Deserializer;
 
+import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 public class GenericRecordSelectorsSuppliers
         implements KeyValueSelectorSuppliersMaker<GenericRecord> {
 
-    private static class AvroNode implements Node<AvroNode> {
+    static class AvroNode implements Node<AvroNode> {
 
         private static final Object NULL_DATA = new Object();
 
-        static AvroNode fromContainer(GenericContainer container) {
-            return new AvroNode(container);
-        }
-
-        static AvroNode fromObject(Object object) {
-            if (object == null) {
-                object = NULL_DATA;
-            }
-            return new AvroNode(object);
-        }
-
-        private static AvroNode of(Object avroNode) {
+        static AvroNode from(String name, Object avroNode) {
             if (avroNode instanceof GenericContainer container) {
                 Schema schema = container.getSchema();
                 Type valueType = schema.getType();
                 return switch (valueType) {
-                    case RECORD, FIXED, ARRAY, ENUM -> fromContainer(container);
-                    default -> fromObject(avroNode);
+                    case RECORD, FIXED, ARRAY, ENUM -> new AvroNode(name, container);
+                    // default -> new AvroNode(name, avroNode);
+                    default -> throw new RuntimeException("Unsupported Avro type: " + valueType);
                 };
             }
-            return fromObject(avroNode);
+            return new AvroNode(name, avroNode);
         }
 
         private final Either<GenericContainer, Object> data;
+        private final String name;
 
-        private AvroNode(GenericContainer container) {
+        private AvroNode(String name, GenericContainer container) {
+            this.name = name;
             data = Either.left(container);
         }
 
-        private AvroNode(Object object) {
-            data = Either.right(object);
+        private AvroNode(String name, Object object) {
+            this.name = name;
+            data = Either.right(Objects.requireNonNullElse(object, NULL_DATA));
+        }
+
+        @Override
+        public String name() {
+            return name;
         }
 
         @Override
@@ -89,14 +90,13 @@ public class GenericRecordSelectorsSuppliers
                 GenericContainer genericContainer = container();
                 Schema schema = genericContainer.getSchema();
                 Type type = schema.getType();
-                return switch (type) {
-                    case RECORD -> {
-                        GenericData.Record record = (GenericData.Record) genericContainer;
-                        yield record.hasField(name);
-                    }
-                    default -> false;
-                };
+                if (type.equals(Type.RECORD)) {
+                    GenericData.Record record = (GenericData.Record) genericContainer;
+                    return record.hasField(name);
+                }
+                return false;
             }
+
             if (object() instanceof Map map) {
                 return map.containsKey(new Utf8(name));
             }
@@ -107,10 +107,16 @@ public class GenericRecordSelectorsSuppliers
         public AvroNode get(String name) {
             if (isContainer()) {
                 GenericData.Record record = (GenericData.Record) container();
-                return AvroNode.of(record.get(name));
+                return AvroNode.from(name, record.get(name));
             }
+
             Map<?, ?> map = (Map<?, ?>) object();
-            return AvroNode.of(map.get(new Utf8(name)));
+            return AvroNode.from(name, map.get(new Utf8(name)));
+        }
+
+        static Data getAsData(GenericData.Record record, String name) {
+            Object obj = record.get(name);
+            return Data.from(name, obj != null ? obj.toString() : (String) null);
         }
 
         @Override
@@ -134,7 +140,11 @@ public class GenericRecordSelectorsSuppliers
         @Override
         public AvroNode get(int index) {
             GenericData.Array<?> array = (GenericData.Array<?>) container();
-            return AvroNode.of(array.get(index));
+            return AvroNode.from(name + "[" + index + "]", array.get(index));
+        }
+
+        static Data getAsData(GenericData.Array<?> array, String name, int index) {
+            return Data.from(name + "[" + index + "]", Objects.toString(array.get(index), null));
         }
 
         @Override
@@ -159,7 +169,78 @@ public class GenericRecordSelectorsSuppliers
         }
 
         @Override
-        public String asText() {
+        public void visit(Consumer<Data> consumer) {
+            if (isContainer()) {
+                Schema schema = container().getSchema();
+                Type type = schema.getType();
+                switch (type) {
+                    case RECORD -> {
+                        GenericData.Record record = (GenericData.Record) container();
+                        for (Schema.Field field : record.getSchema().getFields()) {
+                            consumer.accept(getAsData(record, field.name()));
+                        }
+                    }
+                    case ARRAY -> {
+                        GenericData.Array<?> array = (GenericData.Array<?>) container();
+                        for (int i = 0; i < size(); i++) {
+                            consumer.accept(getAsData(array, name(), i));
+                        }
+                    }
+
+                    case FIXED, ENUM -> consumer.accept(Data.from(name, text()));
+                    default -> {}
+                }
+                return;
+            }
+
+            if (object() instanceof Map map) {
+                for (Object key : map.keySet()) {
+                    consumer.accept(
+                            Data.from(key.toString(), Objects.toString(map.get(key), null)));
+                }
+                return;
+            }
+
+            consumer.accept(Data.from(name, text()));
+        }
+
+        // @Override
+        // public List<AvroNode> fields() {
+        //     if (isContainer()) {
+        //         Schema schema = container().getSchema();
+        //         Type type = schema.getType();
+        //         return switch (type) {
+        //             case RECORD -> {
+        //                 GenericData.Record record = (GenericData.Record) container();
+        //                 List<AvroNode> list =
+        //                         record.getSchema().getFields().stream()
+        //                                 .map(field -> get(field.name()))
+        //                                 .toList();
+        //                 yield list;
+        //             }
+        //             case ARRAY -> {
+        //                 List<AvroNode> array = new ArrayList<>(size());
+        //                 for (int i = 0; i < size(); i++) {
+        //                     array.add(get(i));
+        //                 }
+        //                 yield Collections.unmodifiableList(array);
+        //             }
+        //             default -> Collections.emptyList();
+        //         };
+        //     }
+
+        //     if (object() instanceof Map map) {
+        //         List<AvroNode> fields = new ArrayList<>(map.size());
+        //         for (Object key : map.keySet()) {
+        //             fields.add(get(key.toString()));
+        //         }
+        //         return Collections.unmodifiableList(fields);
+        //     }
+        //     return Collections.emptyList();
+        // }
+
+        @Override
+        public String text() {
             if (isNull()) {
                 return null;
             }
@@ -215,7 +296,7 @@ public class GenericRecordSelectorsSuppliers
         @Override
         public Data extractKey(KafkaRecord<GenericRecord, ?> record, boolean checkScalar)
                 throws ValueException {
-            return super.eval(() -> record.key(), AvroNode::fromContainer, checkScalar);
+            return eval(record::key, AvroNode::new, checkScalar);
         }
     }
 
@@ -251,7 +332,13 @@ public class GenericRecordSelectorsSuppliers
         @Override
         public Data extractValue(KafkaRecord<?, GenericRecord> record, boolean checkScalar)
                 throws ValueException {
-            return super.eval(() -> record.value(), AvroNode::fromContainer, checkScalar);
+            return eval(record::value, AvroNode::new, checkScalar);
+        }
+
+        @Override
+        public Collection<Data> extractValues(KafkaRecord<?, GenericRecord> record)
+                throws ValueException {
+            return evalMulti(record::value, AvroNode::new);
         }
     }
 
