806bc3506b0a1c6b25039e289121742ff8bc18ec
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.come>
2025-10-23 14:39:47 +0200

    Fix import statements for ExtractionExpression and Constant in BaseSelector and ConstantSelectorSupplier

--- kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/common/mapping/selectors/BaseSelector.java
+++ kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/common/mapping/selectors/BaseSelector.java
@@ -17,7 +17,7 @@
 
 package com.lightstreamer.kafka.common.mapping.selectors;
 
-import com.lightstreamer.kafka.common.expressions.Expressions.ExtractionExpression;
+import com.lightstreamer.kafka.common.mapping.selectors.Expressions.ExtractionExpression;
 
 import java.util.Objects;
 
