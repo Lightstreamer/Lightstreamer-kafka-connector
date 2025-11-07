e0f48f5fdeb7a66512e8820acb32c1013248ddfa
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.come>
2025-11-07 19:26:53 +0100

    Add EventListener implementations: introduce SmartEventListener, LegacyEventListener, and RemoteEventListener for enhanced event handling

---
+++ kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/common/listeners/EventListener.java
@@ -0,0 +1,48 @@
+
+/*
+ * Copyright (C) 2025 Lightstreamer Srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.lightstreamer.kafka.common.listeners;
+
+import com.lightstreamer.kafka.common.mapping.Items.SubscribedItem;
+
+import java.util.Map;
+
+public interface EventListener {
+
+    void update(SubscribedItem item, Map<String, String> updates, boolean isSnapshot);
+
+    void clearSnapshot(SubscribedItem item);
+
+    void endOfSnapshot(SubscribedItem item);
+
+    void failure(Exception throwable);
+
+    static EventListener smartEventListener(
+            com.lightstreamer.interfaces.data.ItemEventListener listener) {
+        return new SmartEventListener(listener);
+    }
+
+    static EventListener legacyEventListener(
+            com.lightstreamer.interfaces.data.ItemEventListener listener) {
+        return new LegacyEventListener(listener);
+    }
+
+    static EventListener remoteEventListener(
+            com.lightstreamer.adapters.remote.ItemEventListener listener) {
+        return new RemoteEventListener(listener);
+    }
+}
---
+++ kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/common/listeners/LegacyEventListener.java
@@ -0,0 +1,51 @@
+
+/*
+ * Copyright (C) 2025 Lightstreamer Srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.lightstreamer.kafka.common.listeners;
+
+import com.lightstreamer.interfaces.data.ItemEventListener;
+import com.lightstreamer.kafka.common.mapping.Items.SubscribedItem;
+
+public class LegacyEventListener implements EventListener {
+
+    private final ItemEventListener listener;
+
+    LegacyEventListener(ItemEventListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void update(
+            SubscribedItem item, java.util.Map<String, String> updates, boolean isSnapshot) {
+        listener.update(item.asCanonicalItemName(), updates, isSnapshot);
+    }
+
+    @Override
+    public void clearSnapshot(SubscribedItem sub) {
+        listener.clearSnapshot(sub.asCanonicalItemName());
+    }
+
+    @Override
+    public void endOfSnapshot(SubscribedItem sub) {
+        listener.endOfSnapshot(sub.asCanonicalItemName());
+    }
+
+    @Override
+    public void failure(Exception exception) {
+        listener.failure(exception);
+    }
+}
