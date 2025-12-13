43d4dfff28dc23b14d2a0e8881ab73738f12c078
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.come>
2025-12-13 10:43:12 +0100

    Refactor FieldConfigsTest to enhance static and dynamic field extractor tests

--- kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/common/config/FieldConfigsTest.java
+++ kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/common/config/FieldConfigsTest.java
@@ -18,63 +18,180 @@
 package com.lightstreamer.kafka.common.config;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.lightstreamer.kafka.adapters.mapping.selectors.others.OthersSelectorSuppliers.String;
+import static com.lightstreamer.kafka.test_utils.TestSelectorSuppliers.JsonKeyJsonValue;
+import static com.lightstreamer.kafka.test_utils.TestSelectorSuppliers.JsonValue;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import com.lightstreamer.kafka.adapters.mapping.selectors.others.OthersSelectorSuppliers;
+import com.lightstreamer.kafka.common.mapping.selectors.Expressions.ExtractionExpression;
 import com.lightstreamer.kafka.common.mapping.selectors.ExtractionException;
 import com.lightstreamer.kafka.common.mapping.selectors.FieldsExtractor;
-import com.lightstreamer.kafka.test_utils.TestSelectorSuppliers;
+import com.lightstreamer.kafka.common.mapping.selectors.KeyValueSelectorSuppliers;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class FieldConfigsTest {
 
+    static Stream<Arguments> staticFieldMappings() {
+        return Stream.of(
+                Arguments.of(
+                        Map.of("field1", "#{VALUE}", "field2", "#{KEY}"), String(), false, true),
+                Arguments.of(
+                        Map.of("field1", "#{VALUE.attrib}", "field2", "#{HEADERS}"),
+                        JsonValue(),
+                        true,
+                        false),
+                Arguments.of(
+                        Map.of("field1", "#{VALUE.attrib}", "field2", "#{KEY.attrib}"),
+                        JsonKeyJsonValue(),
+                        true,
+                        true));
+    }
+
     @ParameterizedTest
-    @ValueSource(
-            strings = {
-                "#{VALUE}",
-                "#{OFFSET}",
-                "#{PARTITION}",
-                "#{OFFSET}",
-                "#{TOPIC}",
-                "#{HEADERS}"
-            })
-    void shouldCreateAndMakeExtractor(String expression) throws ExtractionException {
-        Map<String, String> fieldMappings = Map.of("field1", expression);
+    @MethodSource("staticFieldMappings")
+    public void shouldCreateAndMakeStaticFieldsExtractor(
+            Map<String, String> fieldMappings,
+            KeyValueSelectorSuppliers<?, ?> suppliers,
+            boolean mapNonScalars,
+            boolean skipOnFailure)
+            throws ExtractionException {
         FieldConfigs configs = FieldConfigs.from(fieldMappings);
+        checkBoundExpressions(fieldMappings, configs.boundExpressions());
+        assertThat(configs.autoBoundExpressions()).isEmpty();
 
         boolean[] skipOnFailures = {false, true};
-        boolean[] mapScalars = {false, true};
         for (boolean skip : skipOnFailures) {
-            for (boolean mapNonScalars : mapScalars) {
-                FieldsExtractor<String, String> extractor =
-                        configs.fieldsExtractor(
-                                OthersSelectorSuppliers.String(), skip, mapNonScalars);
-                assertThat(extractor.mappedFields()).isEqualTo(fieldMappings.keySet());
-                assertThat(extractor.skipOnFailure()).isEqualTo(skip);
-                assertThat(extractor.mapNonScalars()).isEqualTo(mapNonScalars);
+            FieldsExtractor<?, ?> extractor =
+                    configs.fieldsExtractor(suppliers, skip, mapNonScalars);
+            assertThat(extractor.mappedFields()).isEqualTo(fieldMappings.keySet());
+            assertThat(extractor.skipOnFailure()).isEqualTo(skip);
+            assertThat(extractor.mapNonScalars()).isEqualTo(mapNonScalars);
+        }
+    }
+
+    private void checkBoundExpressions(
+            Map<String, String> fieldMappings, Map<String, ExtractionExpression> boundExpressions) {
+        assertThat(boundExpressions.keySet()).isEqualTo(fieldMappings.keySet());
+        for (Map.Entry<String, ExtractionExpression> entry : boundExpressions.entrySet()) {
+            String fieldName = entry.getKey();
+            ExtractionExpression expr = entry.getValue();
+            String wrapped = fieldMappings.get(fieldName);
+            String unwrapped = wrapped.substring(2, wrapped.length() - 1);
+            assertThat(expr.toString()).isEqualTo(unwrapped);
+        }
+    }
+
+    static Stream<Arguments> dynamicFieldsMapping() {
+        return Stream.of(
+                Arguments.of(Map.of("*", "#{VALUE.list}"), JsonKeyJsonValue(), false),
+                Arguments.of(Map.of("*", "#{VALUE.map}"), JsonKeyJsonValue(), true));
+    }
+
+    @ParameterizedTest
+    @MethodSource("dynamicFieldsMapping")
+    public void shouldCreateAndMakeStaticDynamicExtractor(
+            Map<String, String> fieldMappings,
+            KeyValueSelectorSuppliers<?, ?> suppliers,
+            boolean mapNonScalars)
+            throws ExtractionException {
+        FieldConfigs configs = FieldConfigs.from(fieldMappings);
+        assertThat(configs.boundExpressions()).isEmpty();
+        assertThat(configs.autoBoundExpressions()).hasSize(1);
+
+        boolean[] skipOnFailures = {false, true};
+        for (boolean skip : skipOnFailures) {
+            // The mapNonScalars parameter is ignored for dynamic extractors, as they always map
+            // non-scalar values
+            FieldsExtractor<?, ?> extractor =
+                    configs.fieldsExtractor(suppliers, skip, mapNonScalars);
+            assertThat(extractor.mapNonScalars()).isEqualTo(true);
+            assertThat(extractor.mappedFields()).isEmpty();
+            assertThat(extractor.skipOnFailure()).isEqualTo(skip);
+        }
+    }
+
+    static Stream<Arguments> mixedDynamicFieldsMapping() {
+        return Stream.of(
+                Arguments.of(
+                        Map.of("field1", "#{VALUE}", "field2", "#{KEY}"),
+                        String(),
+                        false, // mapNonScalars
+                        true, // skipOnFailure
+                        false, // expectedComposed
+                        Set.of("field1", "field2")),
+                Arguments.of(
+                        Map.of("*", "#{VALUE.list}"),
+                        JsonKeyJsonValue(),
+                        true, // mapNonScalars
+                        true, // skipOnFailure
+                        false, // expectedComposed
+                        Collections.emptySet()),
+                Arguments.of(
+                        Map.of("*", "#{VALUE.list}", "field1", "#{KEY}"),
+                        JsonKeyJsonValue(),
+                        true, // mapNonScalars
+                        false, // skipOnFailure
+                        true, // expectedComposed
+                        Set.of("field1")));
+    }
+
+    @ParameterizedTest
+    @MethodSource("mixedDynamicFieldsMapping")
+    public void shouldCreateComposedExtractor(
+            Map<String, String> fieldMappings,
+            KeyValueSelectorSuppliers<?, ?> suppliers,
+            boolean mapNonScalars,
+            boolean skipOnFailure,
+            boolean expectedComposed,
+            Set<String> expectedMappedFields)
+            throws ExtractionException {
+
+        // Extract static field mappings for later verification
+        Map<String, String> staticFieldMappings = new HashMap<>();
+        Map<String, String> dynamicFieldMappings = new HashMap<>();
+        for (Map.Entry<String, String> entry : fieldMappings.entrySet()) {
+            if (entry.getKey().equals("*")) {
+                dynamicFieldMappings.put(entry.getKey(), entry.getValue());
+            } else {
+                staticFieldMappings.put(entry.getKey(), entry.getValue());
             }
+        }
+
+        FieldConfigs configs = FieldConfigs.from(fieldMappings);
+        checkBoundExpressions(staticFieldMappings, configs.boundExpressions());
+        assertThat(configs.autoBoundExpressions()).hasSize(dynamicFieldMappings.size());
+
+        FieldsExtractor<?, ?> extractor =
+                configs.fieldsExtractor(JsonKeyJsonValue(), skipOnFailure, mapNonScalars);
+        assertThat(extractor.mappedFields()).isEqualTo(expectedMappedFields);
+        if (!expectedComposed) {
+            assertThat(extractor.skipOnFailure()).isEqualTo(skipOnFailure);
+            assertThat(extractor.mapNonScalars()).isEqualTo(mapNonScalars);
         }
     }
 
     @Test
-    void shouldFailCreateExtractor() {
+    public void shouldFailCreateExtractor() {
         Map<String, String> fieldMappings = Map.of("field1", "#{VALUE.notAllowedAttrib}");
         FieldConfigs configs = FieldConfigs.from(fieldMappings);
         ExtractionException ee =
                 assertThrows(
                         ExtractionException.class,
-                        () ->
-                                configs.fieldsExtractor(
-                                        OthersSelectorSuppliers.String(), false, false));
+                        () -> configs.fieldsExtractor(String(), false, false));
         assertThat(ee)
                 .hasMessageThat()
                 .isEqualTo(
@@ -82,30 +199,10 @@ public class FieldConfigsTest {
     }
 
     @ParameterizedTest
-    @ValueSource(
-            strings = {
-                "#{VALUE}",
-                "#{VALUE.attrib}",
-                "#{OFFSET}",
-                "#{PARTITION}",
-                "#{OFFSET}",
-                "#{TOPIC}",
-                "#{HEADERS}"
-            })
-    void shouldCreateAndMakeExtractorForComplexSupplier(String expression)
-            throws ExtractionException {
-        Map<String, String> fieldMappings = Map.of("field1", expression);
-        FieldConfigs configs = FieldConfigs.from(fieldMappings);
-        FieldsExtractor<Object, Object> extractor =
-                configs.fieldsExtractor(TestSelectorSuppliers.Object(), false, false);
-        assertThat(extractor.mappedFields()).isEqualTo(fieldMappings.keySet());
-    }
-
-    @ParameterizedTest
     @EmptySource
     @NullAndEmptySource
     @ValueSource(strings = {"#{}", ".", "\\", " "})
-    void shouldFailCreationDueToInvalidWrapperExpression(String expression) {
+    public void shouldFailCreationDueToInvalidWrapperExpression(String expression) {
         Map<String, String> fieldMappings = new HashMap<>();
         fieldMappings.put("field1", expression);
         ConfigException ee =
@@ -120,7 +217,7 @@ public class FieldConfigsTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"#{.}", "#{...}"})
-    void shouldFailCreationDueToMissingRootTokens(String fieldExpression) {
+    public void shouldFailCreationDueToMissingRootTokens(String fieldExpression) {
         Map<String, String> fieldMappings = new HashMap<>();
         fieldMappings.put("field1", fieldExpression);
         ConfigException ee =
