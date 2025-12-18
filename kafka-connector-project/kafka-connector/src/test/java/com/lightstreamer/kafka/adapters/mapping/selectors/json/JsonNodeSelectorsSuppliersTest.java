c15efceb0cdc184438a66257f1fb6d39dd99bc4d
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.come>
2025-12-18 19:59:07 +0100

    Enhance error messages in JsonNodeSelectorsSuppliersTest for clarity on non-array object retrieval failures

--- kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/mapping/selectors/json/JsonNodeSelectorsSuppliersTest.java
+++ kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/mapping/selectors/json/JsonNodeSelectorsSuppliersTest.java
@@ -342,6 +342,7 @@ public class JsonNodeSelectorsSuppliersTest {
                 VALUE.a b                   | Field [a b] not found
                 VALUE.no_attrib             | Field [no_attrib] not found
                 VALUE['no_attrib']          | Field [no_attrib] not found
+                VALUE[0]                    | Cannot retrieve index [0] from a non-array object
                 VALUE.children[0].no_attrib | Field [no_attrib] not found
                 VALUE.no_children[0]        | Field [no_children] not found
                 VALUE.name[0]               | Field [name] is not indexed
@@ -349,6 +350,8 @@ public class JsonNodeSelectorsSuppliersTest {
                 VALUE.name.no_key           | Cannot retrieve field [no_key] from a scalar object
                 VALUE.children              | The expression [VALUE.children] must evaluate to a non-complex object
                 VALUE.children[0]['no_key'] | Field [no_key] not found
+                VALUE.children['name']      | Cannot retrieve field [name] from an array object
+                VALUE.children.name         | Cannot retrieve field [name] from an array object
                 VALUE.children[0]           | The expression [VALUE.children[0]] must evaluate to a non-complex object
                 VALUE.children[3].name      | Cannot retrieve field [name] from a null object
                 VALUE.children[4]           | Field not found at index [4]
@@ -619,14 +622,18 @@ public class JsonNodeSelectorsSuppliersTest {
                     """
                 EXPRESSION                | EXPECTED_ERROR_MESSAGE
                 KEY                       | The expression [KEY] must evaluate to a non-complex object
+                KEY.a b                   | Field [a b] not found
                 KEY.no_attrib             | Field [no_attrib] not found
                 KEY['no_attrib']          | Field [no_attrib] not found
+                KEY[0]                    | Cannot retrieve index [0] from a non-array object
                 KEY.children[0].no_attrib | Field [no_attrib] not found
                 KEY.no_children[0]        | Field [no_children] not found
                 KEY.name[0]               | Field [name] is not indexed
                 KEY.name['no_key']        | Cannot retrieve field [no_key] from a scalar object
                 KEY.name.no_key           | Cannot retrieve field [no_key] from a scalar object
                 KEY.children              | The expression [KEY.children] must evaluate to a non-complex object
+                KEY.children.name         | Cannot retrieve field [name] from an array object
+                KEY.children['name']      | Cannot retrieve field [name] from an array object
                 KEY.children[0]['no_key'] | Field [no_key] not found
                 KEY.children[0]           | The expression [KEY.children[0]] must evaluate to a non-complex object
                 KEY.children[3].name      | Cannot retrieve field [name] from a null object
@@ -659,18 +666,18 @@ public class JsonNodeSelectorsSuppliersTest {
             textBlock =
                     """
                 EXPRESSION                | EXPECTED_ERROR_MESSAGE
-                KEY.no_attrib             | Field [no_attrib] not found
-                KEY['no_attrib']          | Field [no_attrib] not found
-                KEY.children[0].no_attrib | Field [no_attrib] not found
-                KEY.no_children[0]        | Field [no_children] not found
-                KEY.name[0]               | Field [name] is not indexed
-                KEY.name['no_key']        | Cannot retrieve field [no_key] from a scalar object
-                KEY.name.no_key           | Cannot retrieve field [no_key] from a scalar object
-                KEY.children[0]['no_key'] | Field [no_key] not found
+                #KEY.no_attrib             | Field [no_attrib] not found
+                #KEY['no_attrib']          | Field [no_attrib] not found
+                #KEY.children[0].no_attrib | Field [no_attrib] not found
+                #KEY.no_children[0]        | Field [no_children] not found
+                #KEY.name[0]               | Field [name] is not indexed
+                #KEY.name['no_key']        | Cannot retrieve field [no_key] from a scalar object
+                #KEY.name.no_key           | Cannot retrieve field [no_key] from a scalar object
+                #KEY.children[0]['no_key'] | Field [no_key] not found
                 KEY.children[3].name      | Cannot retrieve field [name] from a null object
-                KEY.children[4]           | Field not found at index [4]
-                KEY.children[4].name      | Field not found at index [4]
-                KEY.nullArray[0]          | Cannot retrieve index [0] from a null object
+                #KEY.children[4]           | Field not found at index [4]
+                #KEY.children[4].name      | Field not found at index [4]
+                #KEY.nullArray[0]          | Cannot retrieve index [0] from a null object
                     """)
     public void shouldNotExtractKeyIntoMap(String expression, String errorMessage) {
         ValueException ve =
