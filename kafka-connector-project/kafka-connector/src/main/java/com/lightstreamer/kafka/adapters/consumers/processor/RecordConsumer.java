a8087bf734da006c727f75cf66348d2df25ba5c7
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.come>
2026-01-12 19:23:04 +0100

    Refactor RecordConsumer interface to use KafkaRecord and improve record processing methods

--- kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/adapters/consumers/processor/RecordConsumer.java
+++ kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/adapters/consumers/processor/RecordConsumer.java
@@ -17,28 +17,25 @@
 
 package com.lightstreamer.kafka.adapters.consumers.processor;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toList;
-
-import com.lightstreamer.interfaces.data.ItemEventListener;
 import com.lightstreamer.kafka.adapters.config.specs.ConfigTypes.CommandModeStrategy;
 import com.lightstreamer.kafka.adapters.config.specs.ConfigTypes.RecordConsumeWithOrderStrategy;
 import com.lightstreamer.kafka.adapters.config.specs.ConfigTypes.RecordErrorHandlingStrategy;
 import com.lightstreamer.kafka.adapters.consumers.offsets.Offsets.OffsetService;
+import com.lightstreamer.kafka.common.listeners.EventListener;
+import com.lightstreamer.kafka.common.mapping.Items.SubscribedItem;
 import com.lightstreamer.kafka.common.mapping.Items.SubscribedItems;
 import com.lightstreamer.kafka.common.mapping.RecordMapper;
 import com.lightstreamer.kafka.common.mapping.selectors.ValueException;
+import com.lightstreamer.kafka.common.records.KafkaRecord;
+import com.lightstreamer.kafka.common.records.KafkaRecords;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.StreamSupport;
+import java.util.stream.Stream;
 
 public interface RecordConsumer<K, V> {
 
@@ -47,13 +44,13 @@ public interface RecordConsumer<K, V> {
         ORDER_BY_PARTITION(record -> record.topic() + "-" + record.partition()),
         UNORDERED(record -> null);
 
-        private final Function<ConsumerRecord<?, ?>, String> sequence;
+        private final Function<KafkaRecord<?, ?>, String> sequence;
 
-        OrderStrategy(Function<ConsumerRecord<?, ?>, String> sequence) {
+        OrderStrategy(Function<KafkaRecord<?, ?>, String> sequence) {
             this.sequence = sequence;
         }
 
-        String getSequence(ConsumerRecord<?, ?> record) {
+        String getSequence(KafkaRecord<?, ?> record) {
             return sequence.apply(record);
         }
 
@@ -78,7 +75,10 @@ public interface RecordConsumer<K, V> {
             }
         }
 
-        void process(ConsumerRecord<K, V> record) throws ValueException;
+        void process(KafkaRecord<K, V> record) throws ValueException;
+
+        void processAsSnapshot(KafkaRecord<K, V> record, SubscribedItem subscribedItem)
+                throws ValueException;
 
         void useLogger(Logger logger);
 
@@ -101,7 +101,7 @@ public interface RecordConsumer<K, V> {
 
     interface WithEnforceCommandMode<K, V> {
 
-        StartBuildingConsumer<K, V> eventListener(ItemEventListener listener);
+        StartBuildingConsumer<K, V> eventListener(EventListener listener);
     }
 
     public interface StartBuildingConsumer<K, V> {
@@ -139,25 +139,18 @@ public interface RecordConsumer<K, V> {
         return RecordConsumerSupport.startBuildingConsumer(recordProcessor);
     }
 
-    static <K, V> ConsumerRecords<K, V> filter(
-            ConsumerRecords<K, V> records, Predicate<ConsumerRecord<K, V>> predicate) {
-        return new ConsumerRecords<>(
-                StreamSupport.stream(records.spliterator(), false)
-                        .filter(predicate)
-                        .collect(
-                                groupingBy(
-                                        r -> new TopicPartition(r.topic(), r.partition()),
-                                        toList())));
-    }
-
-    default ConsumerRecords<K, V> consumeFilteredRecords(
-            ConsumerRecords<K, V> records, Predicate<ConsumerRecord<K, V>> predicate) {
-        ConsumerRecords<K, V> filtered = filter(records, predicate);
+    default int consumeFilteredRecords(
+            KafkaRecords<K, V> records, Predicate<KafkaRecord<K, V>> predicate) {
+        KafkaRecords<K, V> filtered = records.filter(predicate);
         consumeRecords(filtered);
-        return filtered;
+        return filtered.count();
     }
 
-    void consumeRecords(ConsumerRecords<K, V> records);
+    void consumeRecords(KafkaRecords<K, V> records);
+
+    void consumeRecordsAsSnapshot(KafkaRecords<K, V> records, SubscribedItem subscribedItem);
+
+    default void consumeRecords(Stream<KafkaRecord<K, V>> records) {}
 
     default int numOfThreads() {
         return 1;
