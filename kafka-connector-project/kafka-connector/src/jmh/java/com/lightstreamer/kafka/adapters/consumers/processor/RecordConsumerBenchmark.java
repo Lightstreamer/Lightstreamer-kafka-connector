8863101a1553e9e7e579334e629b1fef87b24e13
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.come>
2026-01-12 19:37:04 +0100

    Add suppress warnings annotation for type casting in RecordConsumerBenchmark setup methods

--- kafka-connector-project/kafka-connector/src/jmh/java/com/lightstreamer/kafka/adapters/consumers/processor/RecordConsumerBenchmark.java
+++ kafka-connector-project/kafka-connector/src/jmh/java/com/lightstreamer/kafka/adapters/consumers/processor/RecordConsumerBenchmark.java
@@ -109,7 +109,10 @@ public class RecordConsumerBenchmark {
         @Setup(Level.Iteration)
         public void setUp() {
             // Reuse the listener and offsetService created in setUpTrial
-            Config<String, JsonNode> config = BenchmarksUtils.newConfigurator(TOPICS, "JSON", 3);
+            @SuppressWarnings("unchecked")
+            Config<String, JsonNode> config =
+                    (Config<String, JsonNode>)
+                            BenchmarksUtils.newConfigurator(TOPICS, "JSON", 3).consumerConfig();
 
             // Configure the RecordMapper.
             RecordMapper<String, JsonNode> recordMapper = BenchmarksUtils.newRecordMapper(config);
@@ -181,8 +184,10 @@ public class RecordConsumerBenchmark {
         @Setup(Level.Iteration)
         public void setUp() {
             // Reuse the listener and offsetService created in setUpTrial
+            @SuppressWarnings("unchecked")
             Config<String, DynamicMessage> config =
-                    BenchmarksUtils.newConfigurator(TOPICS, "PROTOBUF", 3);
+                    (Config<String, DynamicMessage>)
+                            BenchmarksUtils.newConfigurator(TOPICS, "PROTOBUF", 3).consumerConfig();
 
             // Configure the RecordMapper.
             RecordMapper<String, DynamicMessage> recordMapper =
