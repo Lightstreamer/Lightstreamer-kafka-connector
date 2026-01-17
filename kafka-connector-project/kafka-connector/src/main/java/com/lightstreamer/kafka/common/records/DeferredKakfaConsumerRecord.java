1fbb8515c2259839e5ff25f3f1dda86ebbe6967f
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.come>
2026-01-17 13:15:00 +0100

    Add DeferredKafkaConsumerRecord class for handling deferred Kafka records

---
+++ kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/common/records/DeferredKakfaConsumerRecord.java
@@ -0,0 +1,71 @@
+
+/*
+ * Copyright (C) 2025 Lightstreamer Srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.lightstreamer.kafka.common.records;
+
+import com.lightstreamer.kafka.adapters.consumers.deserialization.Deferred;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+final class DeferredKafkaConsumerRecord<K, V> implements KafkaRecord<K, V> {
+
+    private final ConsumerRecord<Deferred<K>, Deferred<V>> record;
+
+    DeferredKafkaConsumerRecord(ConsumerRecord<Deferred<K>, Deferred<V>> record) {
+        this.record = record;
+    }
+
+    @Override
+    public K key() {
+        return record.key().load();
+    }
+
+    @Override
+    public V value() {
+        return record.value().load();
+    }
+
+    @Override
+    public boolean isPayloadNull() {
+        return record.value().isNull();
+    }
+
+    @Override
+    public long timestamp() {
+        return record.timestamp();
+    }
+
+    @Override
+    public long offset() {
+        return record.offset();
+    }
+
+    @Override
+    public String topic() {
+        return record.topic();
+    }
+
+    @Override
+    public int partition() {
+        return record.partition();
+    }
+
+    @Override
+    public KafkaHeaders headers() {
+        return new KafkaHeadersImpl(record.headers());
+    }
+}
