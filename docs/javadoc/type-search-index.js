3fa827d7bc776ca2eaa28b68024319528a1568ea
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2026-01-26 11:21:34 +0100

    Remove references to KafkaConnectorDataAdapterOpts from documentation and update related files

--- docs/javadoc/type-search-index.js
+++ docs/javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.lightstreamer.kafka.adapters.pub","l":"KafkaConnectorMetadataAdapter.KafkaConnectorDataAdapterOpts"},{"p":"com.lightstreamer.kafka.adapters.pub","l":"KafkaConnectorMetadataAdapter"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.lightstreamer.kafka.adapters.pub","l":"KafkaConnectorMetadataAdapter"}];updateSearchResults();
