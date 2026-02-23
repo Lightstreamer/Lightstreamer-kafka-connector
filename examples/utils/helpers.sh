26e8222de61ea09cc03628765fca28fc5d9a12c4
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2026-02-23 17:47:51 +0100

    Remove unused helpers.sh script to streamline project structure

--- examples/utils/helpers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-projectDir="${PWD%/examples/*}/kafka-connector-project"
-
-# Alias to the the local gradlew command
-_gradle="${projectDir}/gradlew --project-dir ${projectDir}"
-
-# Get the current version
-version=$(grep '^version' ${projectDir}/gradle.properties |  awk -F= '{ printf $2}')
