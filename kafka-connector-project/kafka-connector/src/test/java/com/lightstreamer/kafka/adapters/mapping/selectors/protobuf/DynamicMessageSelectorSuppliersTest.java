ef098fd221533d72ab8755f8082fd5fece4af52b
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2026-03-10 11:42:07 +0100

    Refactor DynamicMessageSelectorSuppliersTest: Remove redundant Schema Registry URL parameters

--- kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/mapping/selectors/protobuf/DynamicMessageSelectorSuppliersTest.java
+++ kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/mapping/selectors/protobuf/DynamicMessageSelectorSuppliersTest.java
@@ -72,8 +72,6 @@ public class DynamicMessageSelectorSuppliersTest {
                             "https://localhost:8081",
                             SchemaRegistryConfigs.SCHEMA_REGISTRY_PROVIDER,
                             "CONFLUENT",
-                            SchemaRegistryConfigs.CONFLUENT_URL,
-                            "https://localhost:8081",
                             RECORD_KEY_EVALUATOR_TYPE,
                             PROTOBUF.toString(),
                             RECORD_KEY_EVALUATOR_SCHEMA_REGISTRY_ENABLE,
@@ -123,8 +121,6 @@ public class DynamicMessageSelectorSuppliersTest {
                                 RECORD_KEY_EVALUATOR_SCHEMA_REGISTRY_ENABLE,
                                 "true",
                                 SchemaRegistryConfigs.URL,
-                                "http://localhost:8081",
-                                SchemaRegistryConfigs.CONFLUENT_URL,
                                 "http://localhost:8081"));
         DynamicMessageSelectorSuppliers s = new DynamicMessageSelectorSuppliers(config);
         KeySelectorSupplier<DynamicMessage> keySelectorSupplier = s.makeKeySelectorSupplier();
@@ -203,8 +199,6 @@ public class DynamicMessageSelectorSuppliersTest {
                                 RECORD_VALUE_EVALUATOR_SCHEMA_REGISTRY_ENABLE,
                                 "true",
                                 SchemaRegistryConfigs.URL,
-                                "http://localhost:8081",
-                                SchemaRegistryConfigs.CONFLUENT_URL,
                                 "http://localhost:8081"));
         DynamicMessageSelectorSuppliers s = new DynamicMessageSelectorSuppliers(config);
         ValueSelectorSupplier<DynamicMessage> valueSelectorSupplier = s.makeValueSelectorSupplier();
