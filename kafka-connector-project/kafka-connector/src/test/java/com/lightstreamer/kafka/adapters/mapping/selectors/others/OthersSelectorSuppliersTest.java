87c23c19bfb6eb23e5f5598489dedb603f87d153
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2026-03-10 11:42:47 +0100

    Refactor OthersSelectorSuppliersTest: Remove redundant Confluent Schema Registry URL parameters

--- kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/mapping/selectors/others/OthersSelectorSuppliersTest.java
+++ kafka-connector-project/kafka-connector/src/test/java/com/lightstreamer/kafka/adapters/mapping/selectors/others/OthersSelectorSuppliersTest.java
@@ -119,8 +119,6 @@ public class OthersSelectorSuppliersTest {
                                 RECORD_KEY_EVALUATOR_SCHEMA_REGISTRY_ENABLE,
                                 "true",
                                 SchemaRegistryConfigs.URL,
-                                "http://localhost:8081",
-                                SchemaRegistryConfigs.CONFLUENT_URL,
                                 "http://localhost:8081"));
         OthersSelectorSuppliers s = new OthersSelectorSuppliers(config);
         IllegalArgumentException ie =
@@ -194,8 +192,6 @@ public class OthersSelectorSuppliersTest {
                                 RECORD_VALUE_EVALUATOR_SCHEMA_REGISTRY_ENABLE,
                                 "true",
                                 SchemaRegistryConfigs.URL,
-                                "http://localhost:8081",
-                                SchemaRegistryConfigs.CONFLUENT_URL,
                                 "http://localhost:8081"));
         OthersSelectorSuppliers s = new OthersSelectorSuppliers(config);
         IllegalArgumentException ie =
