5ddb13be96fd47bdbc112ea49b766eaf5da36573
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2026-03-17 17:12:43 +0100

    Refactor start.sh to streamline producer jar build process and simplify service startup

--- examples/airport-demo/utils/start.sh
+++ examples/airport-demo/utils/start.sh
@@ -3,17 +3,19 @@ set -euo pipefail
 WHICH=$1
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "${SCRIPT_DIR}/../../../" && pwd)"
+PRODUCER_DIR="${SCRIPT_DIR}/../producer"
 
 # Load version from shared location
 source "${PROJECT_ROOT}/kafka-connector-project/version.sh"
 
 COMPOSE_FILE="docker-compose-${WHICH}.yml"
 
+# Build the Producer jar, which will be packaged into the Docker image used in the example
+"$PRODUCER_DIR/build.sh"
+
 # Build the Lightstreamer Kafka Connector Docker image
 ${PROJECT_ROOT}/docker/build.sh
 
-if [ $? == 0 ]; then
-     export VERSION
-     docker compose -f ${COMPOSE_FILE} up --build -d
-     echo "Services started. Now you can point your browser to http://localhost:8080/AirportDemo to see real-time data."
- fi
+export VERSION
+docker compose -f "${COMPOSE_FILE}" up --build -d
+echo "Services started. Now you can point your browser to http://localhost:8080/AirportDemo to see real-time data."
