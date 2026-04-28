1b40f2efacebfffbd8e40a829ccccdbba6b21ca9
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2026-04-28 18:53:55 +0200

    Add additional constructor to ConfigException for improved error handling

--- kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/common/config/ConfigException.java
+++ kafka-connector-project/kafka-connector/src/main/java/com/lightstreamer/kafka/common/config/ConfigException.java
@@ -22,4 +22,8 @@ public class ConfigException extends RuntimeException {
     public ConfigException(String message) {
         super(message);
     }
+
+    public ConfigException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
